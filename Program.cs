8f85f4eede866dcb72c16da3213bbe8a08aebc68
Kauã <kaua_hirt@estudante.sesisenai.org.br>
2026-06-15 21:30:54 -0300

    questão 9

--- Program.cs
+++ Program.cs
@@ -1,31 +1,13 @@
-﻿int pontuacao1, pontuacao2, pontuacao3;
+﻿int numero;
 
-        Console.Write("Informe a primeira pontuação: ");
-        pontuacao1 = Convert.ToInt32(Console.ReadLine());
+        Console.Write("Informe um número inteiro: ");
+        numero = Convert.ToInt32(Console.ReadLine());
 
-        Console.Write("Informe a segunda pontuação: ");
-        pontuacao2 = Convert.ToInt32(Console.ReadLine());
-
-        Console.Write("Informe a terceira pontuação: ");
-        pontuacao3 = Convert.ToInt32(Console.ReadLine());
-
-        // Verifica se as três pontuações são iguais
-        if (pontuacao1 == pontuacao2 && pontuacao2 == pontuacao3)
+        if (numero % 2 == 0)
         {
-            Console.WriteLine("As três pontuações são iguais.");
+            Console.WriteLine("O número informado é par.");
         }
-        // Verifica se a primeira é a maior
-        else if (pontuacao1 >= pontuacao2 && pontuacao1 >= pontuacao3)
-        {
-            Console.WriteLine("Maior pontuação: " + pontuacao1);
-        }
-        // Verifica se a segunda é a maior
-        else if (pontuacao2 >= pontuacao1 && pontuacao2 >= pontuacao3)
-        {
-            Console.WriteLine("Maior pontuação: " + pontuacao2);
-        }
-        // Caso contrário, a terceira é a maior
         else
         {
-            Console.WriteLine("Maior pontuação: " + pontuacao3);
+            Console.WriteLine("O número informado é ímpar.");
         }
