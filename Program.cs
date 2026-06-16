0c75e5dd21de8ba53b3085f926a3b392a4415ee9
Kauã <kaua_hirt@estudante.sesisenai.org.br>
2026-06-15 21:23:33 -0300

    questão 8

--- Program.cs
+++ Program.cs
@@ -1,20 +1,31 @@
-﻿ int numero1, numero2;
+﻿int pontuacao1, pontuacao2, pontuacao3;
 
-        Console.Write("Informe o primeiro número: ");
-        numero1 = Convert.ToInt32(Console.ReadLine());
+        Console.Write("Informe a primeira pontuação: ");
+        pontuacao1 = Convert.ToInt32(Console.ReadLine());
 
-        Console.Write("Informe o segundo número: ");
-        numero2 = Convert.ToInt32(Console.ReadLine());
+        Console.Write("Informe a segunda pontuação: ");
+        pontuacao2 = Convert.ToInt32(Console.ReadLine());
 
-        if (numero1 > numero2)
+        Console.Write("Informe a terceira pontuação: ");
+        pontuacao3 = Convert.ToInt32(Console.ReadLine());
+
+        // Verifica se as três pontuações são iguais
+        if (pontuacao1 == pontuacao2 && pontuacao2 == pontuacao3)
         {
-            Console.WriteLine("O primeiro número é maior.");
+            Console.WriteLine("As três pontuações são iguais.");
         }
-        else if (numero2 > numero1)
+        // Verifica se a primeira é a maior
+        else if (pontuacao1 >= pontuacao2 && pontuacao1 >= pontuacao3)
         {
-            Console.WriteLine("O segundo número é maior.");
+            Console.WriteLine("Maior pontuação: " + pontuacao1);
         }
+        // Verifica se a segunda é a maior
+        else if (pontuacao2 >= pontuacao1 && pontuacao2 >= pontuacao3)
+        {
+            Console.WriteLine("Maior pontuação: " + pontuacao2);
+        }
+        // Caso contrário, a terceira é a maior
         else
         {
-            Console.WriteLine("Os números são iguais.");
+            Console.WriteLine("Maior pontuação: " + pontuacao3);
         }
